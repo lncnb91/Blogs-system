51082898549936028b25a21af26985d30f8c8476
lecuong <le.ngoc.cuong@framgia.com>
2014-04-24 12:52:29 +0700

    entry title attribute changed

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :title, presence: true, length: {maximum: 70}
+  validates :title, presence: true, length: {maximum: 140}
   validates :content, presence: true
   validates :user_id, presence: true
 end
